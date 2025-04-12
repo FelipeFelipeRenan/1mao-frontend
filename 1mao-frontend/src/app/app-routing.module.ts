e523fb8ab51c3ec3a54118be2a0338e9a063b5d4
felipe.fernandes@payco.com.br <feliperenanqwerty@gmail.com>
2025-04-12 19:01:59 -0300

    refact: refactoring pages and components directories

--- 1mao-frontend/src/app/app-routing.module.ts
+++ 1mao-frontend/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' }
+  { path: '**', redirectTo: '/login' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- 1mao-frontend/src/app/app.module.ts
+++ 1mao-frontend/src/app/app.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
