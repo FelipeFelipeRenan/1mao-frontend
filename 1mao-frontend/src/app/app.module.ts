f06594d870628dd2c5543a3394f88a719e58f18d
felipe.fernandes@payco.com.br <feliperenanqwerty@gmail.com>
2025-04-12 18:59:14 -0300

    fix: fixing login page redirection

---
+++ 1mao-frontend/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
